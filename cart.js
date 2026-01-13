744c3078f0f7669e4081133293bc214fdd5a4c9c
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2026-01-13 10:04:35 +0530

    Fixed cart logic and branding for VentureMond

--- cart.js
+++ cart.js
@@ -1,143 +1,53 @@
-console.clear();
+let cartContainer = document.getElementById('cartContainer');
+let cartData = JSON.parse(localStorage.getItem('venturemond_cart')) || [];
 
-if(document.cookie.indexOf(',counter=')>=0)
-{
-    let counter = document.cookie.split(',')[1].split('=')[1]
-    document.getElementById("badge").innerHTML = counter
+function dynamicCartSection(ob, count) {
+    let boxDiv = document.createElement('div');
+    boxDiv.className = 'cart-item-box'; // Custom class for styling
+    boxDiv.style.display = "flex";
+    boxDiv.style.alignItems = "center";
+    boxDiv.style.margin = "10px 0";
+
+    boxDiv.innerHTML = `
+        <img src="${ob.preview}" style="width: 80px; border-radius: 5px; margin-right: 15px;">
+        <div>
+            <h3>${ob.name} (x${count})</h3>
+            <h4>Amount: Rs ${ob.price * count}</h4>
+        </div>
+    `;
+    return boxDiv;
 }
 
+// Backend Call to get full product info for items in cart
+let httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status == 200) {
+        let allProducts = JSON.parse(this.responseText);
+        let totalAmount = 0;
+        cartContainer.innerHTML = ""; // Clear loader
 
-let cartContainer = document.getElementById('cartContainer')
+        // Logic to count duplicates
+        let counts = {};
+        cartData.forEach(id => { counts[id] = (counts[id] || 0) + 1; });
 
-let boxContainerDiv = document.createElement('div')
-boxContainerDiv.id = 'boxContainer'
-
-// DYNAMIC CODE TO SHOW THE SELECTED ITEMS IN YOUR CART
-function dynamicCartSection(ob,itemCounter)
-{
-    let boxDiv = document.createElement('div')
-    boxDiv.id = 'box'
-    boxContainerDiv.appendChild(boxDiv)
-
-    let boxImg = document.createElement('img')
-    boxImg.src = ob.preview
-    boxDiv.appendChild(boxImg)
-
-    let boxh3 = document.createElement('h3')
-    let h3Text = document.createTextNode(ob.name + ' × ' + itemCounter)
-    // let h3Text = document.createTextNode(ob.name)
-    boxh3.appendChild(h3Text)
-    boxDiv.appendChild(boxh3)
-
-    let boxh4 = document.createElement('h4')
-    let h4Text = document.createTextNode('Amount: Rs' + ob.price)
-    boxh4.appendChild(h4Text)
-    boxDiv.appendChild(boxh4)
-
-    // console.log(boxContainerDiv);
-
-    buttonLink.appendChild(buttonText)
-    cartContainer.appendChild(boxContainerDiv)
-    cartContainer.appendChild(totalContainerDiv)
-    // let cartMain = document.createElement('div')
-    // cartmain.id = 'cartMainContainer'
-    // cartMain.appendChild(totalContainerDiv)
-
-    return cartContainer
-}
-
-let totalContainerDiv = document.createElement('div')
-totalContainerDiv.id = 'totalContainer'
-
-let totalDiv = document.createElement('div')
-totalDiv.id = 'total'
-totalContainerDiv.appendChild(totalDiv)
-
-let totalh2 = document.createElement('h2')
-let h2Text = document.createTextNode('Total Amount')
-totalh2.appendChild(h2Text)
-totalDiv.appendChild(totalh2)
-
-// TO UPDATE THE TOTAL AMOUNT
-function amountUpdate(amount)
-{
-    let totalh4 = document.createElement('h4')
-    // let totalh4Text = document.createTextNode(amount)
-    let totalh4Text = document.createTextNode('Amount: Rs ' + amount)
-    totalh4Text.id = 'toth4'
-    totalh4.appendChild(totalh4Text)
-    totalDiv.appendChild(totalh4)
-    totalDiv.appendChild(buttonDiv)
-    console.log(totalh4);
-}
-
-
-let buttonDiv = document.createElement('div')
-buttonDiv.id = 'button'
-totalDiv.appendChild(buttonDiv)
-
-let buttonTag = document.createElement('button')
-buttonDiv.appendChild(buttonTag)
-
-let buttonLink = document.createElement('a')
-buttonLink.href = '/orderPlaced.html?'
-buttonTag.appendChild(buttonLink)
-
-buttonText = document.createTextNode('Place Order')
-buttonTag.onclick = function()
-{
-    console.log("clicked")
-}  
-//dynamicCartSection()
-// console.log(dynamicCartSection());
-
-// BACKEND CALL
-let httpRequest = new XMLHttpRequest()
-let totalAmount = 0
-httpRequest.onreadystatechange = function()
-{
-    if(this.readyState === 4)
-    {
-        if(this.status == 200)
-        {
-            // console.log('call successful');
-            contentTitle = JSON.parse(this.responseText)
-
-            let counter = Number(document.cookie.split(',')[1].split('=')[1])
-            document.getElementById("totalItem").innerHTML = ('Total Items: ' + counter)
-
-            let item = document.cookie.split(',')[0].split('=')[1].split(" ")
-            console.log(counter)
-            console.log(item)
-
-            let i;
-            let totalAmount = 0
-            for(i=0; i<counter; i++)
-            {
-                let itemCounter = 1
-                for(let j = i+1; j<counter; j++)
-                {   
-                    if(Number(item[j]) == Number(item[i]))
-                    {
-                        itemCounter +=1;
-                    }
-                }
-                totalAmount += Number(contentTitle[item[i]-1].price) * itemCounter
-                dynamicCartSection(contentTitle[item[i]-1],itemCounter)
-                i += (itemCounter-1)
+        Object.keys(counts).forEach(id => {
+            let product = allProducts.find(p => p.id == id);
+            if(product) {
+                totalAmount += (product.price * counts[id]);
+                cartContainer.appendChild(dynamicCartSection(product, counts[id]));
             }
-            amountUpdate(totalAmount)
-        }
+        });
+
+        document.getElementById("totalItem").innerHTML = 'Total Items: ' + cartData.length;
+        document.getElementById("toth4").innerHTML = 'Amount: Rs ' + totalAmount;
     }
-        else
-        {
-            console.log('call failed!');
-        }
-}
+};
 
-httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product', true)
-httpRequest.send()
+httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product', true);
+httpRequest.send();
 
-
-
-
+// Place Order Logic
+document.getElementById('place-order-btn').onclick = function() {
+    localStorage.removeItem('venturemond_cart'); // Clear cart
+    location.href = 'orderPlaced.html';
+};
--- content.js
+++ content.js
@@ -1,36 +1,24 @@
-// console.clear();
-
-let contentTitle;
-
-console.log(document.cookie);
 function dynamicClothingSection(ob) {
   let boxDiv = document.createElement("div");
   boxDiv.id = "box";
 
   let boxLink = document.createElement("a");
-  // boxLink.href = '#'
-  boxLink.href = "/contentDetails.html?" + ob.id;
-  // console.log('link=>' + boxLink);
+  boxLink.href = "contentDetails.html?" + ob.id;
 
   let imgTag = document.createElement("img");
-  // imgTag.id = 'image1'
-  // imgTag.id = ob.photos
   imgTag.src = ob.preview;
 
   let detailsDiv = document.createElement("div");
   detailsDiv.id = "details";
 
   let h3 = document.createElement("h3");
-  let h3Text = document.createTextNode(ob.name);
-  h3.appendChild(h3Text);
+  h3.innerHTML = ob.name;
 
   let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
-  h4.appendChild(h4Text);
+  h4.innerHTML = ob.brand;
 
   let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
-  h2.appendChild(h2Text);
+  h2.innerHTML = "Rs " + ob.price;
 
   boxDiv.appendChild(boxLink);
   boxLink.appendChild(imgTag);
@@ -42,50 +30,29 @@ function dynamicClothingSection(ob) {
   return boxDiv;
 }
 
-//  TO SHOW THE RENDERED CODE IN CONSOLE
-// console.log(dynamicClothingSection());
-
-// console.log(boxDiv)
-
-let mainContainer = document.getElementById("mainContainer");
 let containerClothing = document.getElementById("containerClothing");
 let containerAccessories = document.getElementById("containerAccessories");
-// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
 
-// BACKEND CALLING
-
+// Fetch Products from API
 let httpRequest = new XMLHttpRequest();
-
 httpRequest.onreadystatechange = function() {
-  if (this.readyState === 4) {
-    if (this.status == 200) {
-      // console.log('call successful');
-      contentTitle = JSON.parse(this.responseText);
-      if (document.cookie.indexOf(",counter=") >= 0) {
-        var counter = document.cookie.split(",")[1].split("=")[1];
-        document.getElementById("badge").innerHTML = counter;
+  if (this.readyState === 4 && this.status == 200) {
+      let products = JSON.parse(this.responseText);
+      
+      // Update Badge from LocalStorage
+      let cart = JSON.parse(localStorage.getItem('venturemond_cart')) || [];
+      if(document.getElementById("badge")) {
+          document.getElementById("badge").innerHTML = cart.length;
       }
-      for (let i = 0; i < contentTitle.length; i++) {
-        if (contentTitle[i].isAccessory) {
-          console.log(contentTitle[i]);
-          containerAccessories.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
+
+      for (let i = 0; i < products.length; i++) {
+        if (products[i].isAccessory) {
+          containerAccessories.appendChild(dynamicClothingSection(products[i]));
         } else {
-          console.log(contentTitle[i]);
-          containerClothing.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
+          containerClothing.appendChild(dynamicClothingSection(products[i]));
         }
       }
-    } else {
-      console.log("call failed!");
-    }
   }
 };
-httpRequest.open(
-  "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
-  true
-);
+httpRequest.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/product", true);
 httpRequest.send();
